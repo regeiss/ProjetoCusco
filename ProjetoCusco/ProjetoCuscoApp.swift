9f67350d874acfe7639fdcd487340a3a6bbc522f
Roberto <roberto.geiss.nh@icloud.com>
2024-06-02 10:29:11 -0300

    Initial Commit

---
+++ ProjetoCusco/ProjetoCuscoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoCuscoApp.swift
+//  ProjetoCusco
+//
+//  Created by Roberto Edgar Geiss on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoCuscoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
