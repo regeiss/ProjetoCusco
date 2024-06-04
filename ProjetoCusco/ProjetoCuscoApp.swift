298cccfa3cf227587fbfcab82abb3acf208e5aad
Roberto <roberto.geiss.nh@icloud.com>
2024-06-03 21:36:58 -0300

    inicial

--- ProjetoCusco/ProjetoCuscoApp.swift
+++ ProjetoCusco/ProjetoCuscoApp.swift
@@ -6,11 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate 
+{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool
+    {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 
 @main
-struct ProjetoCuscoApp: App {
-    var body: some Scene {
-        WindowGroup {
+struct ProjetoCuscoApp: App 
+{
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    var body: some Scene
+    {
+        WindowGroup 
+        {
             ContentView()
         }
     }
